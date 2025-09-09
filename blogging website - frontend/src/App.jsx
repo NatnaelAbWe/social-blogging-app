140a50f596712f3c78742e9be5d8d692a96289d1
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-09-09 21:37:14 +0300

    refactor: use Routes on on app jsx

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signIn" element={<h1>Sign In</h1>} />
+        <Route path="signUp" element={<h1>Sign Up</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
