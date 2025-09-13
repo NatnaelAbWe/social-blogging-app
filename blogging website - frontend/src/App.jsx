618de3f0ad169f9351fd5348a6fc6d069fa3e60d
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-09-13 21:02:21 +0300

    feat: render components

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import UserAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="signIn" element={<h1>Sign In</h1>} />
-        <Route path="signUp" element={<h1>Sign Up</h1>} />
+        <Route path="signIn" element={<UserAuthForm type="sign-in" />} />
+        <Route path="signUp" element={<UserAuthForm type="sign-up" />} />
       </Route>
     </Routes>
   );
